fdc12bcdfb360ea1bd899ec879edbad5b5273b2b
Adam Góra <adam.gora@viperprint.pl>
2020-03-02 13:47:26 +0100

    Registered homepage and passed it into context

--- tests/Behat/Context/Ui/Homepage/BrowsingHomepageContext.php
+++ tests/Behat/Context/Ui/Homepage/BrowsingHomepageContext.php
@@ -2,10 +2,21 @@
 
 namespace App\Tests\Behat\Context\Ui\Homepage;
 
+use App\Tests\Behat\Page\Shop\Homepage\HomePageInterface;
 use Behat\Behat\Context\Context;
 
 class BrowsingHomepageContext implements Context
 {
+    /**
+     * @var HomePageInterface
+     */
+    private $homePage;
+
+    public function __construct(HomePageInterface $homePage)
+    {
+        $this->homePage = $homePage;
+    }
+
     /**
      * @Then the alert with class :className should be visible
      */
