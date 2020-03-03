552b8295b4d036162406e5e3d8050ceec565541a
Adam Góra <adam.gora@viperprint.pl>
2020-03-03 10:32:15 +0100

    Implemented methods to find elements on page

--- tests/Behat/Page/Shop/Homepage/HomePageInterface.php
+++ tests/Behat/Page/Shop/Homepage/HomePageInterface.php
@@ -5,4 +5,6 @@ namespace App\Tests\Behat\Page\Shop\Homepage;
 interface HomePageInterface extends \Sylius\Behat\Page\Shop\HomePageInterface
 {
     public function getAlertWithClass(string $className);
+    public function waitForProductButtonToAppear(string $className);
+    public function clickOnButton(string $className);
 }
