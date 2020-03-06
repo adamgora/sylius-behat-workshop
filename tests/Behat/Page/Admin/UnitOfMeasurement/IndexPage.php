67060494e0f93aee133b885f18a480950a010643
Adam Góra <adam.gora@viperprint.pl>
2020-03-06 10:59:17 +0100

    Created index page

---
+++ tests/Behat/Page/Admin/UnitOfMeasurement/IndexPage.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests\Behat\Page\Admin\Subcontractor;
+
+use Sylius\Behat\Page\Admin\Crud\IndexPage as BaseIndexPage;
+
+class IndexPage extends BaseIndexPage implements IndexPageInterface
+{
+}
---
+++ tests/Behat/Page/Admin/UnitOfMeasurement/IndexPageInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Tests\Behat\Page\Admin\Subcontractor;
+
+
+use Sylius\Behat\Page\Admin\Crud\IndexPageInterface as BaseIndexPageInterface;
+
+interface IndexPageInterface extends BaseIndexPageInterface
+{
+}
